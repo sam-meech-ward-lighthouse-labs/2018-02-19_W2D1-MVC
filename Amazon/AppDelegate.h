c5d8cd4ce05de33feb101e2a3af20d884a35d233
Sam Meech-Ward <sam@meech-ward.com>
2018-02-19 09:02:07 -0800

    Initial Commit

---
+++ Amazon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Amazon
+//
+//  Created by Sam Meech-Ward on 2018-02-19.
+//  Copyright © 2018 lighthouse-labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
